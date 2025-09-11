de6e23bb52f032eef9c70082b7b198a8da71b69d
drench <drench+github@gmail.com>
2025-09-10 21:41:38 -0500

    Update record_spec to make standard happy

--- spec/fixy/record_spec.rb
+++ spec/fixy/record_spec.rb
@@ -1,10 +1,8 @@
-require "spec_helper"
-
-describe "Defining a Record" do
+describe Fixy::Record do
   context "when the definition is correct" do
     it "raises no exception" do
       expect {
-        class PersonRecord < Fixy::Record
+        Class.new(described_class) do
           include Fixy::Formatter::Alphanumeric
 
           set_record_length 20
@@ -21,14 +19,14 @@ describe "Defining a Record" do
   context "when the definition is incorrect" do
     it "raises an appropriate exception" do
       expect {
-        class PersonRecordA < Fixy::Record
+        Class.new(described_class) do
           set_record_length 20
           field :first_name, 10, "1-10", :alphanumeric
         end
       }.to raise_error(ArgumentError, "Unknown type 'alphanumeric'")
 
       expect {
-        class PersonRecordB < Fixy::Record
+        Class.new(described_class) do
           include Fixy::Formatter::Alphanumeric
 
           set_record_length 20
@@ -37,7 +35,7 @@ describe "Defining a Record" do
       }.to raise_error(ArgumentError, "Invalid Range (size: 2, range: 1-10)")
 
       expect {
-        class PersonRecordC < Fixy::Record
+        Class.new(described_class) do
           include Fixy::Formatter::Alphanumeric
 
           set_record_length 20
@@ -47,7 +45,7 @@ describe "Defining a Record" do
       }.to raise_error(ArgumentError, "Column 1 has already been allocated")
 
       expect {
-        class PersonRecordD < Fixy::Record
+        Class.new(described_class) do
           include Fixy::Formatter::Alphanumeric
 
           set_record_length 10
@@ -61,7 +59,197 @@ end
 
 describe "Generating a Record" do
   context "when properly defined" do
-    class PersonRecordE < Fixy::Record
+    let(:person_record_e) do
+      Class.new(Fixy::Record) do
+        include Fixy::Formatter::Alphanumeric
+
+        set_record_length 20
+
+        field :first_name, 10, "1-10", :alphanumeric
+        field :last_name, 10, "11-20", :alphanumeric
+
+        field_value :first_name, -> { "Sarah" }
+
+        def last_name
+          "Kerrigan"
+        end
+      end
+    end
+
+    it "generates a fixed width record" do
+      expect(person_record_e.new.generate).to eq "Sarah     Kerrigan  \n"
+    end
+
+    context "when using the debug flag" do
+      it "produces a debug log" do
+        expect(person_record_e.new.generate(true))
+          .to eq File.read("spec/fixtures/debug_record.txt")
+      end
+    end
+  end
+
+  context "when dealing with multi-byte characters" do
+    let(:person_record_multibyte) do
+      Class.new(Fixy::Record) do
+        include Fixy::Formatter::Alphanumeric
+
+        set_record_length 9
+
+        field :name, 9, "1-9", :alphanumeric
+
+        field_value :name, -> { "12345678И" }
+      end
+    end
+
+    it "generates a fixed width record" do
+      value = person_record_multibyte.new.generate
+      expect(value).to be_valid_encoding
+      expect(value).to eq "12345678 \n"
+    end
+  end
+
+  context "when a field value is nil" do
+    let(:person_record_nil) do
+      Class.new(Fixy::Record) do
+        include Fixy::Formatter::Alphanumeric
+
+        set_record_length 9
+
+        field :name, 9, "1-9", :alphanumeric
+
+        field_value :name, -> {}
+      end
+    end
+
+    it "emits spaces" do
+      value = person_record_nil.new.generate
+      expect(value).to eq "         \n"
+    end
+  end
+
+  context "when a field value contains the record separator" do
+    let(:person_record_new_line) do
+      Class.new(Fixy::Record) do
+        include Fixy::Formatter::Alphanumeric
+
+        set_record_length 9
+
+        field :name, 9, "1-9", :alphanumeric
+
+        field_value :name, -> { "Two\nLine" }
+      end
+    end
+
+    it "strips that separator" do
+      value = person_record_new_line.new.generate
+      expect(value).to eq "TwoLine  \n"
+    end
+  end
+
+  context "when definition is incomplete (e.g. undefined columns)" do
+    let(:person_record_f) do
+      Class.new(Fixy::Record) do
+        include Fixy::Formatter::Alphanumeric
+
+        set_record_length 20
+        field :first_name, 10, "1-10", :alphanumeric
+        field :last_name, 8, "11-18", :alphanumeric
+        field_value :first_name, -> { "Sarah" }
+        field_value :last_name, -> { "Kerrigan" }
+      end
+    end
+
+    it "raises an error" do
+      expect { person_record_f.new.generate }
+        .to raise_error(StandardError, "Undefined field for position 19")
+    end
+  end
+
+  context "when inheriting from another record" do
+    let(:person_record_g) do
+      Class.new(Fixy::Record) do
+        include Fixy::Formatter::Alphanumeric
+
+        set_record_length 20
+
+        field :first_name, 10, "1-10", :alphanumeric
+        field_value :first_name, -> { "Bob" }
+      end
+    end
+
+    let(:person_record_h) do
+      Class.new(person_record_g) do
+        include Fixy::Formatter::Alphanumeric
+
+        set_record_length 20
+        field :last_name, 10, "11-20", :alphanumeric
+        field_value :last_name, -> { "Williams" }
+      end
+    end
+
+    it "includes fields from the superclass" do
+      expect(person_record_h.new.generate.slice(0, 10)).to eq "Bob       "
+      expect(person_record_h.new.generate.slice(10, 10)).to eq "Williams  "
+    end
+
+    context "when two records inherit" do
+      let(:person_record_i) do
+        Class.new(person_record_g) do
+          include Fixy::Formatter::Alphanumeric
+
+          set_record_length 20
+          field :last_name, 10, "11-20", :alphanumeric
+          field_value :last_name, -> { "Jacobs" }
+        end
+      end
+
+      it "does not collide" do
+        expect(person_record_h.new.generate.slice(0, 10)).to eq "Bob       "
+        expect(person_record_h.new.generate.slice(10, 10)).to eq "Williams  "
+
+        expect(person_record_i.new.generate.slice(0, 10)).to eq "Bob       "
+        expect(person_record_i.new.generate.slice(10, 10)).to eq "Jacobs    "
+      end
+    end
+  end
+
+  context "when a block is passed to field" do
+    let(:person_record_j) do
+      Class.new(Fixy::Record) do
+        include Fixy::Formatter::Alphanumeric
+
+        set_record_length 20
+        field(:description, 20, "1-20", :alphanumeric) { "Use My Value" }
+      end
+    end
+
+    it "uses the proc conversion as the field value" do
+      expect(person_record_j.new.generate).to eq("Use My Value".ljust(20) << "\n")
+    end
+  end
+
+  context "when setting a line ending" do
+    let(:person_record_with_line_ending) do
+      Class.new(Fixy::Record) do
+        include Fixy::Formatter::Alphanumeric
+
+        set_record_length 20
+        set_line_ending Fixy::Record::LINE_ENDING_CRLF
+        field(:description, 20, "1-20", :alphanumeric) { "Use My Value" }
+      end
+    end
+
+    it "uses the given line ending" do
+      expect(person_record_with_line_ending.new.generate)
+        .to eq("Use My Value".ljust(20) << "\r\n")
+    end
+  end
+end
+
+describe "Parsing a record" do
+  let(:multibyte_record) { "älimuk   Karil     " }
+  let(:person_record_e) do
+    Class.new(Fixy::Record) do
       include Fixy::Formatter::Alphanumeric
 
       set_record_length 20
@@ -75,163 +263,11 @@ describe "Generating a Record" do
         "Kerrigan"
       end
     end
-
-    it "generates a fixed width record" do
-      expect(PersonRecordE.new.generate).to eq "Sarah     Kerrigan  \n"
-    end
-
-    context "when using the debug flag" do
-      it "produces a debug log" do
-        expect(PersonRecordE.new.generate(true)).to eq File.read("spec/fixtures/debug_record.txt")
-      end
-    end
   end
 
-  context "when dealing with multi-byte characters" do
-    it "generates a fixed width record" do
-      class PersonRecordMultibyte < Fixy::Record
-        include Fixy::Formatter::Alphanumeric
-
-        set_record_length 9
-
-        field :name, 9, "1-9", :alphanumeric
-
-        field_value :name, -> { "12345678И" }
-      end
-
-      value = PersonRecordMultibyte.new.generate
-      expect(value).to be_valid_encoding
-      expect(value).to eq "12345678 \n"
-    end
-  end
-
-  context "when a field value is nil" do
-    it "emits spaces" do
-      class PersonRecordNil < Fixy::Record
-        include Fixy::Formatter::Alphanumeric
-
-        set_record_length 9
-
-        field :name, 9, "1-9", :alphanumeric
-
-        field_value :name, -> {}
-      end
-
-      value = PersonRecordNil.new.generate
-      expect(value).to eq "         \n"
-    end
-  end
-
-  context "when a field value contains the record separator" do
-    it "strips that separator" do
-      class PersonRecordNewLine < Fixy::Record
-        include Fixy::Formatter::Alphanumeric
-
-        set_record_length 9
-
-        field :name, 9, "1-9", :alphanumeric
-
-        field_value :name, -> { "Two\nLine" }
-      end
-
-      value = PersonRecordNewLine.new.generate
-      expect(value).to eq "TwoLine  \n"
-    end
-  end
-
-  context "when definition is incomplete (e.g. undefined columns)" do
-    it "raises an error" do
-      class PersonRecordF < Fixy::Record
-        include Fixy::Formatter::Alphanumeric
-
-        set_record_length 20
-        field :first_name, 10, "1-10", :alphanumeric
-        field :last_name, 8, "11-18", :alphanumeric
-        field_value :first_name, -> { "Sarah" }
-        field_value :last_name, -> { "Kerrigan" }
-      end
-
-      expect { PersonRecordF.new.generate }
-        .to raise_error(StandardError, "Undefined field for position 19")
-    end
-  end
-
-  context "when inheriting from another record" do
-    class PersonRecordG < Fixy::Record
-      include Fixy::Formatter::Alphanumeric
-
-      set_record_length 20
-
-      field :first_name, 10, "1-10", :alphanumeric
-      field_value :first_name, -> { "Bob" }
-    end
-
-    class PersonRecordH < PersonRecordG
-      include Fixy::Formatter::Alphanumeric
-
-      set_record_length 20
-      field :last_name, 10, "11-20", :alphanumeric
-      field_value :last_name, -> { "Williams" }
-    end
-
-    it "includes fields from the superclass" do
-      expect(PersonRecordH.new.generate.slice(0, 10)).to eq "Bob       "
-      expect(PersonRecordH.new.generate.slice(10, 10)).to eq "Williams  "
-    end
-
-    context "when two records inherit" do
-      class PersonRecordI < PersonRecordG
-        include Fixy::Formatter::Alphanumeric
-
-        set_record_length 20
-        field :last_name, 10, "11-20", :alphanumeric
-        field_value :last_name, -> { "Jacobs" }
-      end
-
-      it "does not collide" do
-        expect(PersonRecordH.new.generate.slice(0, 10)).to eq "Bob       "
-        expect(PersonRecordH.new.generate.slice(10, 10)).to eq "Williams  "
-
-        expect(PersonRecordI.new.generate.slice(0, 10)).to eq "Bob       "
-        expect(PersonRecordI.new.generate.slice(10, 10)).to eq "Jacobs    "
-      end
-    end
-  end
-
-  context "when a block is passed to field" do
-    class PersonRecordJ < Fixy::Record
-      include Fixy::Formatter::Alphanumeric
-
-      set_record_length 20
-      field(:description, 20, "1-20", :alphanumeric) { "Use My Value" }
-    end
-
-    it "uses the proc conversion as the field value" do
-      expect(PersonRecordJ.new.generate).to eq("Use My Value".ljust(20) << "\n")
-    end
-  end
-
-  context "when setting a line ending" do
-    class PersonRecordWithLineEnding < Fixy::Record
-      include Fixy::Formatter::Alphanumeric
-
-      set_record_length 20
-      set_line_ending Fixy::Record::LINE_ENDING_CRLF
-      field(:description, 20, "1-20", :alphanumeric) { "Use My Value" }
-    end
-
-    it "uses the given line ending" do
-      expect(PersonRecordWithLineEnding.new.generate)
-        .to eq("Use My Value".ljust(20) << "\r\n")
-    end
-  end
-end
-
-describe "Parsing a record" do
-  let(:multibyte_record) { "älimuk   Karil     " }
   context "with a record of multi-byte characters" do
     it "does not raise an error with the right number of bytes" do
-      expect(PersonRecordE.parse(multibyte_record, true)).to eq(
+      expect(person_record_e.parse(multibyte_record, true)).to eq(
         record: File.read("spec/fixtures/debug_parsed_multibyte_record.txt"),
         fields: [
           {name: :first_name, value: "älimuk   "},
@@ -241,16 +277,25 @@ describe "Parsing a record" do
     end
 
     it "raises an error with the wrong number of characters" do
-      expect { PersonRecordE.parse("älimuk  Karil     ", true) }
+      expect { person_record_e.parse("älimuk  Karil     ", true) }
         .to raise_error(StandardError, "Record length is invalid (Expected 20)")
     end
   end
 
   context "with custom line endings" do
+    let(:person_record_with_line_ending) do
+      Class.new(Fixy::Record) do
+        include Fixy::Formatter::Alphanumeric
+
+        set_record_length 20
+        set_line_ending Fixy::Record::LINE_ENDING_CRLF
+        field(:description, 20, "1-20", :alphanumeric) { "Use My Value" }
+      end
+    end
     let(:record) { "Use My Value        " }
 
     it "generates a fixed width record" do
-      expect(PersonRecordWithLineEnding.parse(record)).to eq(
+      expect(person_record_with_line_ending.parse(record)).to eq(
         record: (record + Fixy::Record::LINE_ENDING_CRLF),
         fields: [{name: :description, value: "Use My Value        "}]
       )
@@ -259,23 +304,25 @@ describe "Parsing a record" do
 
   context "when properly defined" do
     let(:record) { "Sarah     Kerrigan  " }
-    class PersonRecordK < Fixy::Record
-      include Fixy::Formatter::Alphanumeric
+    let(:person_record_k) do
+      Class.new(Fixy::Record) do
+        include Fixy::Formatter::Alphanumeric
 
-      set_record_length 20
+        set_record_length 20
 
-      field :first_name, 10, "1-10", :alphanumeric
-      field :last_name, 10, "11-20", :alphanumeric
+        field :first_name, 10, "1-10", :alphanumeric
+        field :last_name, 10, "11-20", :alphanumeric
 
-      field_value :first_name, -> { "Sarah" }
+        field_value :first_name, -> { "Sarah" }
 
-      def last_name
-        "Kerrigan"
+        def last_name
+          "Kerrigan"
+        end
       end
     end
 
     it "generates a fixed width record" do
-      expect(PersonRecordE.parse(record)).to eq(
+      expect(person_record_e.parse(record)).to eq(
         record: (record + "\n"),
         fields: [
           {name: :first_name, value: "Sarah     "},
@@ -286,7 +333,7 @@ describe "Parsing a record" do
 
     context "when using the debug flag" do
       it "produces a debug log" do
-        expect(PersonRecordE.parse(record, true)).to eq(
+        expect(person_record_e.parse(record, true)).to eq(
           record: File.read("spec/fixtures/debug_parsed_record.txt"),
           fields: [
             {name: :first_name, value: "Sarah     "},
@@ -299,14 +346,14 @@ describe "Parsing a record" do
     context "when invalid record provided" do
       context "with a non-string record type" do
         it "raises an error" do
-          expect { PersonRecordE.parse(nil, true) }
+          expect { person_record_e.parse(nil, true) }
             .to raise_error(StandardError, "Record must be a string")
         end
       end
 
       context "with an invalid record length" do
         it "raises an error" do
-          expect { PersonRecordE.parse("", true) }
+          expect { person_record_e.parse("", true) }
             .to raise_error(StandardError, "Record length is invalid (Expected 20)")
         end
       end
@@ -314,8 +361,8 @@ describe "Parsing a record" do
   end
 
   context "when definition is incomplete (e.g. undefined columns)" do
-    it "raises an error" do
-      class PersonRecordL < Fixy::Record
+    let(:person_record_l) do
+      Class.new(Fixy::Record) do
         include Fixy::Formatter::Alphanumeric
 
         set_record_length 20
@@ -324,8 +371,10 @@ describe "Parsing a record" do
         field_value :first_name, -> { "Sarah" }
         field_value :last_name, -> { "Kerrigan" }
       end
+    end
 
-      expect { PersonRecordL.parse(" " * 20) }
+    it "raises an error" do
+      expect { person_record_l.parse(" " * 20) }
         .to raise_error(StandardError, "Undefined field for position 19")
     end
   end
