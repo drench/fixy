22a130a96443a2c759072c6082fd0d4e9e213684
drench <drench+github@gmail.com>
2025-09-11 21:35:05 -0500

    Tidy the record specs

--- spec/fixy/record_spec.rb
+++ spec/fixy/record_spec.rb
@@ -105,7 +105,7 @@ end
 
 describe "Generating a Record" do
   context "when properly defined" do
-    let(:person_record_e) do
+    let(:person_record) do
       Class.new(Fixy::Record) do
         include Fixy::Formatter::Alphanumeric
 
@@ -122,20 +122,22 @@ describe "Generating a Record" do
       end
     end
 
-    it "generates a fixed width record" do
-      expect(person_record_e.new.generate).to eq "Sarah     Kerrigan  \n"
+    context "without the debug flag" do
+      subject { person_record.new.generate }
+      it { is_expected.to eq "Sarah     Kerrigan  \n" }
     end
 
-    context "when using the debug flag" do
+    context "with the debug flag" do
+      subject { person_record.new.generate(true) }
+
       it "produces a debug log" do
-        expect(person_record_e.new.generate(true))
-          .to eq File.read("spec/fixtures/debug_record.txt")
+        is_expected.to eq File.read("spec/fixtures/debug_record.txt")
       end
     end
   end
 
   context "when dealing with multi-byte characters" do
-    let(:person_record_multibyte) do
+    let(:person_record) do
       Class.new(Fixy::Record) do
         include Fixy::Formatter::Alphanumeric
 
@@ -147,11 +149,9 @@ describe "Generating a Record" do
       end
     end
 
-    it "generates a fixed width record" do
-      value = person_record_multibyte.new.generate
-      expect(value).to be_valid_encoding
-      expect(value).to eq "12345678 \n"
-    end
+    subject { person_record.new.generate }
+
+    it { is_expected.to be_valid_encoding.and eq "12345678 \n" }
   end
 
   context "when a field value is a string" do
@@ -172,7 +172,7 @@ describe "Generating a Record" do
   end
 
   context "when a field value is nil" do
-    let(:person_record_nil) do
+    let(:person_record) do
       Class.new(Fixy::Record) do
         include Fixy::Formatter::Alphanumeric
 
@@ -184,14 +184,15 @@ describe "Generating a Record" do
       end
     end
 
+    subject { person_record.new.generate }
+
     it "emits spaces" do
-      value = person_record_nil.new.generate
-      expect(value).to eq "         \n"
+      is_expected.to eq "         \n"
     end
   end
 
   context "when a field value contains the record separator" do
-    let(:person_record_new_line) do
+    let(:person_record) do
       Class.new(Fixy::Record) do
         include Fixy::Formatter::Alphanumeric
 
@@ -203,14 +204,15 @@ describe "Generating a Record" do
       end
     end
 
+    subject { person_record.new.generate }
+
     it "strips that separator" do
-      value = person_record_new_line.new.generate
-      expect(value).to eq "TwoLine  \n"
+      is_expected.to eq "TwoLine  \n"
     end
   end
 
   context "when definition is incomplete (e.g. undefined columns)" do
-    let(:person_record_f) do
+    let(:person_record) do
       Class.new(Fixy::Record) do
         include Fixy::Formatter::Alphanumeric
 
@@ -223,7 +225,7 @@ describe "Generating a Record" do
     end
 
     it "raises an error" do
-      expect { person_record_f.new.generate }
+      expect { person_record.new.generate }
         .to raise_error(StandardError, "Undefined field for position 19")
     end
   end
@@ -277,7 +279,7 @@ describe "Generating a Record" do
   end
 
   context "when a block is passed to field" do
-    let(:person_record_j) do
+    let(:person_record) do
       Class.new(Fixy::Record) do
         include Fixy::Formatter::Alphanumeric
 
@@ -286,13 +288,15 @@ describe "Generating a Record" do
       end
     end
 
+    subject { person_record.new.generate }
+
     it "uses the proc conversion as the field value" do
-      expect(person_record_j.new.generate).to eq("Use My Value".ljust(20) << "\n")
+      is_expected.to eq("Use My Value".ljust(20) << "\n")
     end
   end
 
   context "when setting a line ending" do
-    let(:person_record_with_line_ending) do
+    let(:person_record) do
       Class.new(Fixy::Record) do
         include Fixy::Formatter::Alphanumeric
 
@@ -302,16 +306,16 @@ describe "Generating a Record" do
       end
     end
 
+    subject { person_record.new.generate }
+
     it "uses the given line ending" do
-      expect(person_record_with_line_ending.new.generate)
-        .to eq("Use My Value".ljust(20) << "\r\n")
+      is_expected.to eq("Use My Value".ljust(20) << "\r\n")
     end
   end
 end
 
 describe "Parsing a record" do
-  let(:multibyte_record) { "älimuk   Karil     " }
-  let(:person_record_e) do
+  let(:person_record) do
     Class.new(Fixy::Record) do
       include Fixy::Formatter::Alphanumeric
 
@@ -329,24 +333,34 @@ describe "Parsing a record" do
   end
 
   context "with a record of multi-byte characters" do
-    it "does not raise an error with the right number of bytes" do
-      expect(person_record_e.parse(multibyte_record, true)).to eq(
-        record: File.read("spec/fixtures/debug_parsed_multibyte_record.txt"),
-        fields: [
-          {name: :first_name, value: "älimuk   "},
-          {name: :last_name, value: "Karil     "}
-        ]
-      )
+    subject { person_record.parse(multibyte_record, true) }
+
+    context "with the right number of bytes" do
+      let(:multibyte_record) { "älimuk   Karil     " }
+
+      it "does not raise an error with the right number of bytes" do
+        is_expected.to eq(
+          record: File.read("spec/fixtures/debug_parsed_multibyte_record.txt"),
+          fields: [
+            {name: :first_name, value: "älimuk   "},
+            {name: :last_name, value: "Karil     "}
+          ]
+        )
+      end
     end
 
-    it "raises an error with the wrong number of characters" do
-      expect { person_record_e.parse("älimuk  Karil     ", true) }
-        .to raise_error(StandardError, "Record length is invalid (Expected 20)")
+    context "with the wrong number of bytes" do
+      let(:multibyte_record) { "älimuk  Karil     " }
+
+      it "raises an error with the wrong number of characters" do
+        expect { subject }
+          .to raise_error(StandardError, "Record length is invalid (Expected 20)")
+      end
     end
   end
 
   context "with custom line endings" do
-    let(:person_record_with_line_ending) do
+    let(:person_record) do
       Class.new(Fixy::Record) do
         include Fixy::Formatter::Alphanumeric
 
@@ -356,9 +370,10 @@ describe "Parsing a record" do
       end
     end
     let(:record) { "Use My Value        " }
+    subject { person_record.parse(record) }
 
     it "generates a fixed width record" do
-      expect(person_record_with_line_ending.parse(record)).to eq(
+      is_expected.to eq(
         record: (record + Fixy::Record::LINE_ENDING_CRLF),
         fields: [{name: :description, value: "Use My Value        "}]
       )
@@ -367,7 +382,7 @@ describe "Parsing a record" do
 
   context "when properly defined" do
     let(:record) { "Sarah     Kerrigan  " }
-    let(:person_record_k) do
+    let(:person_record) do
       Class.new(Fixy::Record) do
         include Fixy::Formatter::Alphanumeric
 
@@ -385,7 +400,7 @@ describe "Parsing a record" do
     end
 
     it "generates a fixed width record" do
-      expect(person_record_e.parse(record)).to eq(
+      expect(person_record.parse(record)).to eq(
         record: (record + "\n"),
         fields: [
           {name: :first_name, value: "Sarah     "},
@@ -396,7 +411,7 @@ describe "Parsing a record" do
 
     context "when using the debug flag" do
       it "produces a debug log" do
-        expect(person_record_e.parse(record, true)).to eq(
+        expect(person_record.parse(record, true)).to eq(
           record: File.read("spec/fixtures/debug_parsed_record.txt"),
           fields: [
             {name: :first_name, value: "Sarah     "},
@@ -409,14 +424,14 @@ describe "Parsing a record" do
     context "when invalid record provided" do
       context "with a non-string record type" do
         it "raises an error" do
-          expect { person_record_e.parse(nil, true) }
+          expect { person_record.parse(nil, true) }
             .to raise_error(StandardError, "Record must be a string")
         end
       end
 
       context "with an invalid record length" do
         it "raises an error" do
-          expect { person_record_e.parse("", true) }
+          expect { person_record.parse("", true) }
             .to raise_error(StandardError, "Record length is invalid (Expected 20)")
         end
       end
@@ -424,7 +439,7 @@ describe "Parsing a record" do
   end
 
   context "when definition is incomplete (e.g. undefined columns)" do
-    let(:person_record_l) do
+    let(:person_record) do
       Class.new(Fixy::Record) do
         include Fixy::Formatter::Alphanumeric
 
@@ -436,8 +451,10 @@ describe "Parsing a record" do
       end
     end
 
+    subject { person_record.parse(" " * 20) }
+
     it "raises an error" do
-      expect { person_record_l.parse(" " * 20) }
+      expect { subject }
         .to raise_error(StandardError, "Undefined field for position 19")
     end
   end
